57aac7db78697a3f4e35d7992a992b9be3da21d2
user <user@WIN-M1JADIG1GJ0>
2025-06-04 12:12:39 +0300

    Overload for double DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -14,10 +14,16 @@ void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
 
 int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+
 double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
 
 int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
+
 int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
 
 void ShiftLeft(int arr[],const int n, const int number_of_shifts);
 void ShiftRight(int arr[],const int n, const int number_of_shifts);
@@ -46,9 +52,13 @@ void main()
 	const int SIZE = 8;
 	double brr[SIZE];
 	FillRand(brr, SIZE);
-	//Print(brr, SIZE);
+	Print(brr, SIZE);
 	Sort(brr, SIZE);
-	//Print(brr, SIZE);
+	Print(brr, SIZE);
+	cout << "Сумма элементов массива: " << Sum(brr, SIZE) << endl;
+	cout << "Среднее арифметическое элементов массива: " << Avg(brr, SIZE) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(brr, SIZE) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(brr, SIZE) << endl;
 }
 
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -126,10 +136,25 @@ int Sum(int arr[], const int n)
 	}
 	return Sum;
 }
+double Sum(double arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+
 double Avg(int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
+double Avg(double arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+
 int minValueIn(int arr[], const int n)
 {
 	int min = arr[0];
@@ -139,6 +164,16 @@ int minValueIn(int arr[], const int n)
 	}
 	return min;
 }
+double minValueIn(double arr[], const int n)
+{
+	double min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+
 int maxValueIn(int arr[], const int n)
 {
 	int max = arr[0];
@@ -148,6 +183,16 @@ int maxValueIn(int arr[], const int n)
 	}
 	return max;
 }
+double maxValueIn(double arr[], const int n)
+{
+	double max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+
 void ShiftLeft(int arr[], const int n, const int number_of_shifts)
 {
 	for (int i = 0; i < number_of_shifts; i++)
