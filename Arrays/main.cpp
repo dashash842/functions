14c7da40bbdbfe506476b7d68b6485c80677b2af
user <user@WIN-M1JADIG1GJ0>
2025-06-04 11:35:35 +0300

    int functions DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+//void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
 
 void Print(int arr[], const int n);
 void Print(double arr[], const int n);
